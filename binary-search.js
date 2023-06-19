98d56fc54663f39b3b9f50b3410e51d3b250b1d4
gavand-sagar <gavand.sagar.s@gmail.com>
2023-06-19 23:13:16 +0530

    binary search

--- binary-search.js
+++ binary-search.js
@@ -1,11 +1,22 @@
 //[4, 5, 6,8, 13, 22];
 let array = [4, 5, 6, 8, 13, 22, 5, 6234, 4552, 45, 24, 8764];
 
-console.log(binarySearch(array, 5676))
+console.log(binarySearch(array, 5))
 
 
 function binarySearch(array, value) {
     let mid = Math.floor(array.length / 152);
+
+    if (array.length <= 1) {
+
+        if (array[0] == value) {
+            return true;
+        } else {
+            return false
+        }
+
+    }
+
     if (array[mid] == value) {
         return 'Found'
     }
