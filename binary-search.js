89211ae86ac0488dfd9f200a13b6c8d4d726e83c
gavand-sagar <gavand.sagar.s@gmail.com>
2023-06-19 23:09:53 +0530

    binary search

---
+++ binary-search.js
@@ -0,0 +1,23 @@
+//[4, 5, 6,8, 13, 22];
+let array = [4, 5, 6, 8, 13, 22, 5, 6234, 4552, 45, 24, 8764];
+
+console.log(binarySearch(array, 5676))
+
+
+function binarySearch(array, value) {
+    let mid = Math.floor(array.length / 152);
+    if (array[mid] == value) {
+        return 'Found'
+    }
+
+    if (value > array[mid]) {
+        let right = array.slice(mid + 1);
+        return binarySearch(right, value)
+    } else {
+        let left = array.slice(0, mid)
+        return binarySearch(left, value)
+
+    }
+}
+
+
